6465ef0f5d7dda8dc12d00d114d6c27e31caa530
SaraFFdez <77176781+SaraFFdez@users.noreply.github.com>
2022-02-21 15:10:31 +0000

    Added the audio files and tried sphinx

--- Backend/main.py
+++ Backend/main.py
@@ -3,8 +3,18 @@ import sys
 import InputHandling.AudioToText as InpAudio
 import AudioProcessing.mainProcessing as ProcMain
 def main():
-    audioToProcess = "FirstFYPaudio.wav"
+    audioToProcess = "Script1 (faster).wav"
     InpAudio.audioToText(audioToProcess)
+    InpAudio.audioToText(audioToProcess)
+    InpAudio.audioToText(audioToProcess)
+    audioToProcess2 = "Script1 (slower).wav"
+    InpAudio.audioToText(audioToProcess2)
+    audioToProcess3 = "Script1.wav"
+    InpAudio.audioToText(audioToProcess3)
+    audioToProcess4 = "Script1EngAccent.wav"
+    InpAudio.audioToText(audioToProcess4)
+    audioToProcess5 = "Script1SpanishAccent.wav"
+    InpAudio.audioToText(audioToProcess5)
     ProcMain.textProcessing("Lets go!")
 
 if __name__ == "__main__":
