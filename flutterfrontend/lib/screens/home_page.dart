c29bca8960b319afeb2ef7dda30222f9cb042274
SaraFFdez <77176781+SaraFFdez@users.noreply.github.com>
2022-04-26 16:03:34 +0100

    progress bar added (not working)

--- flutterfrontend/lib/screens/home_page.dart
+++ flutterfrontend/lib/screens/home_page.dart
@@ -12,11 +12,21 @@ class _HomePageState extends State<HomePage> {
   String _timeString = "00:00";
   String _dateString = "00/00/00";
   late Timer _timer;
+  double _progressValue = 0.5;
+
+  String getCorrectTime() {
+    String minutes = "";
+    if (DateTime.now().minute < 10) {
+      minutes = "0" + DateTime.now().minute.toString();
+    } else {
+      minutes = DateTime.now().minute.toString();
+    }
+    return DateTime.now().hour.toString() + ":" + minutes;
+  }
 
   @override
   void initState() {
-    _timeString =
-        DateTime.now().hour.toString() + ":" + DateTime.now().minute.toString();
+    _timeString = getCorrectTime();
     _dateString = DateTime.now().day.toString() +
         "/" +
         DateTime.now().month.toString() +
@@ -40,8 +50,7 @@ class _HomePageState extends State<HomePage> {
         "/" +
         DateTime.now().year.toString();
 
-    String time =
-        DateTime.now().hour.toString() + ":" + DateTime.now().minute.toString();
+    String time = getCorrectTime();
 
     setState(() {
       _timeString = time;
@@ -61,39 +70,70 @@ class _HomePageState extends State<HomePage> {
         centerTitle: true,
         backgroundColor: Colors.green[100],
       ),
-      body: Column(crossAxisAlignment: CrossAxisAlignment.stretch, children: [
-        Row(
+      body: Column(
+          //crossAxisAlignment: CrossAxisAlignment.stretch,
           children: [
-            const Expanded(
-              flex: 1,
-              child: SizedBox(),
-            ),
             Expanded(
-              flex: 3,
-              child: Container(
-                padding: const EdgeInsets.all(40),
-                color: Colors.white,
-                child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Text(
-                        _timeString,
-                        style: const TextStyle(
-                            fontSize: 30), //change the font later
+                flex: 1,
+                child: Row(
+                  children: [
+                    const Expanded(
+                      flex: 1,
+                      child: SizedBox(),
+                    ),
+                    Expanded(
+                      flex: 3,
+                      child: Container(
+                        padding: const EdgeInsets.all(40),
+                        color: Colors.white,
+                        child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Text(
+                                _timeString,
+                                style: const TextStyle(
+                                    fontSize: 30), //change the font later
+                              ),
+                              const SizedBox(height: 5.0),
+                              Text(_dateString)
+                            ]), //if we find a way to center this in the row, yay
                       ),
-                      const SizedBox(height: 5.0),
-                      Text(_dateString)
-                    ]), //if we find a way to center this in the row, yay
-              ),
-            ),
-          ],
-        ),
-        Container(
-          padding: const EdgeInsets.fromLTRB(0.0, 200.0, 0.0, 200.0),
-          color: Colors.blue,
-          child: const Text("Here the progress bar"),
-        ),
-      ]),
+                    ),
+                  ],
+                )),
+            Expanded(
+                flex: 3,
+                child: Row(
+                  children: [
+                    const Expanded(flex: 1, child: SizedBox()),
+                    Expanded(
+                        flex: 6,
+                        child: SizedBox(
+                            height: 350.0,
+                            width: 200.0,
+                            child: Stack(
+                              children: [
+                                Center(
+                                  child: SizedBox(
+                                    height: 350.0,
+                                    width: 350.0,
+                                    child: CircularProgressIndicator(
+                                      value: _progressValue,
+                                      backgroundColor: Colors.grey,
+                                      valueColor:
+                                          const AlwaysStoppedAnimation<Color>(
+                                              Colors.green),
+                                      strokeWidth: 30.0,
+                                    ),
+                                  ),
+                                ),
+                                Center(child: Text("Progress, $_progressValue"))
+                              ],
+                            ))),
+                    const Expanded(flex: 1, child: SizedBox()),
+                  ],
+                ))
+          ]),
     );
   }
 }
--- flutterfrontend/lib/screens/notes_page.dart
+++ flutterfrontend/lib/screens/notes_page.dart
@@ -10,8 +10,18 @@ class Notes extends StatefulWidget {
 class _NotesState extends State<Notes> {
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Text("This is the notes page"),
+    return Scaffold(
+      body: Column(
+        children: [
+          Center(
+            child: CircularProgressIndicator(
+              value: 0.5,
+              strokeWidth: 20.0,
+            ),
+          ),
+          Text("here is the thing")
+        ],
+      ),
     );
   }
 }
