a9a9814ce54bd0028fc76f25a3dc45817fccb9fa
SaraFFdez <77176781+SaraFFdez@users.noreply.github.com>
2022-05-21 17:40:01 +0100

    uploading data to MongoDB

--- Backend/main.py
+++ Backend/main.py
@@ -4,22 +4,28 @@ import InputHandling.AudioToText as InpAudio
 import AudioProcessing.mainProcessing as ProcMain
 import InputHandling.change_audio_format as getAudio
 import time
+import pymongo
+import datetime
 
 def main():
-    #tic = time.perf_counter()
-    audioToProcess = str(getAudio.find_audio_wav())
-    print(audioToProcess)
-    transcript = InpAudio.AIspeechToTex(audioToProcess)
-    print("Transcript: ", transcript)
-    if transcript == "Error":
-      print("There was an error")
-    else:
-      symptomsList, foodDiary, activityList = ProcMain.speech_processing_main(transcript)
-      print("SYMPTOM LIST:", symptomsList)
-      print("FOOD DIARY DICTIONARY:", foodDiary)
-      print("ACTIVITY LIST:", activityList)
     
-    
+  #tic = time.perf_counter()
+  audioToProcess = str(getAudio.find_audio_wav())
+  print(audioToProcess)
+  transcript = InpAudio.AIspeechToTex(audioToProcess)
+  print("Transcript: ", transcript)
+  if transcript == "Error":
+    print("There was an error")
+  else:
+    client = pymongo.MongoClient("mongodb+srv://sf3218:1Jo41mJdyA6XLeDm@cluster0.ayhp3hd.mongodb.net/?retryWrites=true&w=majority")
+    db = client.test
+    symptomsList, foodDiary, activityList = ProcMain.speech_processing_main(transcript)
+    print("SYMPTOM LIST:", symptomsList)
+    print("FOOD DIARY DICTIONARY:", foodDiary)
+    print("ACTIVITY LIST:", activityList)
+    data = {"SYMPTOM LIST": symptomsList, "FOOD DIARY DICTIONARY": foodDiary, "ACTIVITY LIST": activityList, "status": "sent"}
+    FYPset = db.FYP
+    results = FYPset.insert_one(data)
     #toc = time.perf_counter()
     #print(f"Time taken: {toc - tic:0.4f} seconds")
     
